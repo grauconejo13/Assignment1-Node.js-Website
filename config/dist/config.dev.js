b68b8bc7f6592346c9cc44919b4f073cbcedfaeb
grauconejo13 <vanessaf.victorino@yahoo.com>
2022-10-01 16:16:27 -0500

    Putting together the structure!

---
+++ config/dist/config.dev.js
@@ -0,0 +1,10 @@
+"use strict";
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+exports.MongoURI = exports.Secret = void 0;
+var Secret = "MySecret";
+exports.Secret = Secret;
+var MongoURI = "mongodb://127.0.0.1/media";
+exports.MongoURI = MongoURI;
